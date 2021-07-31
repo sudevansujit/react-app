209d12272a33822c8c981435e5be427cc7b02d53
Sujit S <dhruvjit1311@gmail.com>
2021-07-31 18:27:49 +0530

    new header

---
+++ my-app/src/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div >
+      <h1 className = 'Title'>React App</h1>
+    </div>
+  )
+}
+
+export default Title
